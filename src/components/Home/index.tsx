a2f53860591475c6231e9c6a1b5f8ab4fbe98e88
Sam Hirst <SamuelJHirst@outlook.com>
2022-10-29 09:51:45 +0100

    Update home page text

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -31,7 +31,7 @@ function Home() {
         <h1>Hi, I'm Sam.</h1>
         <h2>Welcome to My Website!</h2>
         <br />
-        <p>I am a web developer who likes to make stuff! This is my portfolio where I upload my projects - finished, in progress or abandoned. You'll also find my blog here too.</p>
+        <p>I am a web developer who likes to make stuff! This is my portfolio where I upload my projects - finished, in progress or abandoned.</p>
         <p>Want to know more? Click the About Me button on the sidebar!</p>
       </div>
     </BackgroundOverlay>
