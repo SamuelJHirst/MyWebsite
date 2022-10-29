ff72c7385956d1836077309d0101031654de49dc
Sam Hirst <SamuelJHirst@outlook.com>
2022-10-29 10:12:54 +0100

    Update GitHub link in sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -116,7 +116,7 @@ function Sidebar(props: SidebarProps) {
       </ButtonGroup>
       <Divider />
       <div className={styles.icons}> 
-        <IconButton href="https://github.com/Naeviant" className={styles.iconButton} target="_blank" rel="noreferrer">
+        <IconButton href="https://github.com/SamuelJHirst" className={styles.iconButton} target="_blank" rel="noreferrer">
           <GitHubIcon className={styles.icon} />
         </IconButton>
         {/* <IconButton href="https://twitter.com/Naeviant" className={styles.iconButton} target="_blank" rel="noreferrer">
